96fe26ba33a3d55e2c21c99bbb3497f6bc96bcf1
Julio Olivencia <julioolivencia891@gmail.com>
2021-03-25 19:44:22 -0300

    la terminal sigue abierta luego de ejecutar

--- Terminal/main.c
+++ Terminal/main.c
@@ -1,22 +1,44 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<wait.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char**argv) {
+void terminal() {
+
     char buff[100];
     int res = 0;
+    pid_t p;
     printf("PROMPT$ ");
     scanf("%s", buff);
-    // "/usr/bin/ls" = buff
-    printf("%s", buff);
-    char* args[] = { buff, NULL};
-    
-    res = execv(buff, args);
-
-    if( res < 0){
-        perror("Algo pasó!");
+    int status = 0;
+    char* args[2];
+    while(strcmp(buff, "exit") != 0) {
+        p = fork();
+        if (p < 0) {
+            perror("Error al forkear!");
+            return;
+        }
+        if (p == 0) {
+            args[0] = buff;
+            args[1] = NULL;
+            res = execv(buff, args);
+            if( res < 0)
+                perror("Error al execvear!");
+            exit(-1);
+        }
+        wait(&status);
+        if (WIFEXITED(status)) {
+            printf("PROMPT$ ");
+            scanf("%s", buff);
+        } 
+        else 
+            perror("Error retornar el nene!");
+        
     }
+}
 
+int main(int argc, char**argv) {
+    terminal();
     return 0;
 }
