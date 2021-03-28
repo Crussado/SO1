f48074d06049d195e26e4166af01823172e597f9
Julio Olivencia <julioolivencia891@gmail.com>
2021-03-28 14:47:10 -0300

    se permite ingresar &

--- Terminal/main.c
+++ Terminal/main.c
@@ -4,13 +4,28 @@
 #include <wait.h>
 #include <stdlib.h>
 
-void agarrarArg(char* buff, char* * args[]) {
-    char delim[2] = " ";
-    *args[0] = strtok(buff, delim); //ruta 
-    for (int i = 0; *args[i] != NULL; i++) {
-        *args[i + 1] = strtok(NULL, delim); //demas arg.
-    }
-}
+// void agarrarArg(char* buff[], char** args[]) {
+//     char delim[2] = " ";
+//     *args[0] = strtok(buff, delim); //ruta 
+//     for (int i = 0; *args[i] != NULL; i++) {
+//         *args[i + 1] = strtok(NULL, delim); //demas arg.
+//     }
+// }
+
+// void llamada(char* buff[], int* esperar) {
+//     printf("PROMPT$ ");
+//     fgets(buff, 50, stdin);
+//     int len = strlen(buff);
+
+//     if(buff[len - 2] == '&') {
+//         esperar = 0;
+//         buff[len - 3] = '\0';
+//     }
+//     else {
+//         esperar = 1;
+//         buff[len - 1] = '\0';
+//     }
+// }
 
 void terminal() {
     char buff[100];
@@ -19,37 +34,72 @@ void terminal() {
     int status = 0;
     char* args[10];
     char delim[2] = " ";
+    int esperar = 1;
 
     printf("PROMPT$ ");
     fgets(buff, 50, stdin);
     int len = strlen(buff);
-    buff[len - 1] = '\0';
+
+    if(buff[len - 2] == '&') {
+        esperar = 0;
+        buff[len - 3] = '\0';
+    }
+    else {
+        esperar = 1;
+        buff[len - 1] = '\0';
+    }
+
     while(strcmp(buff, "exit") != 0) {
         p = fork();
         if (p < 0) {
             perror("Error al forkear!");
             return;
         }
+
         if (p == 0) {
             printf("%s", buff);
             args[0] = strtok(buff, delim); // ruta 
-            for (int i = 0; args[i] != NULL; i++) {
+            for (int i = 0; args[i] != NULL; i++)
                 args[i + 1] = strtok(NULL, delim); //demas arg.
-            }
             res = execv(args[0], args);
             if( res < 0)
                 perror("Error al execvear!");
-            exit(1);
+            exit(-1);
         }
-        wait(&status); // pid = 5
-        if (WIFEXITED(status)) {
+
+        if(esperar) {
+            wait(&status);
+            
             printf("PROMPT$ ");
             fgets(buff, 50, stdin);
             len = strlen(buff);
-            buff[len - 1] = '\0';
-        } 
-        else
-            perror("Error retornar el nene!");
+
+            if(buff[len - 2] == '&') {
+                esperar = 0;
+                buff[len - 3] = '\0';
+            }
+            else {
+                esperar = 1;
+                buff[len - 1] = '\0';
+            }
+
+            if (!WIFEXITED(status))
+                perror("Error retornar el nene!");
+       }
+        else {
+            printf("PROMPT$ ");
+            fgets(buff, 50, stdin);
+            len = strlen(buff);
+
+            if(buff[len - 2] == '&') {
+                esperar = 0;
+                buff[len - 3] = '\0';
+            }
+            else {
+                esperar = 1;
+                buff[len - 1] = '\0';
+            }
+        }
     }
 }
 
