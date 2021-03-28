074cb06c26760c7d8030d9f3a82a5fa9e6678e41
Julio Olivencia <julioolivencia891@gmail.com>
2021-03-28 13:58:45 -0300

    se permiten recibir argumentos

--- Terminal/main.c
+++ Terminal/main.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <wait.h>
 #include <stdlib.h>
-#include <string.h>
+
+void agarrarArg(char* buff, char* * args[]) {
+    char delim[2] = " ";
+    *args[0] = strtok(buff, delim); //ruta 
+    for (int i = 0; *args[i] != NULL; i++) {
+        *args[i + 1] = strtok(NULL, delim); //demas arg.
+    }
+}
 
 void terminal() {
-
     char buff[100];
     int res = 0;
     pid_t p;
-    printf("PROMPT$ ");
-    scanf("%s", buff);
     int status = 0;
-    char* args[2];
+    char* args[10];
+    char delim[2] = " ";
+
+    printf("PROMPT$ ");
+    fgets(buff, 50, stdin);
+    int len = strlen(buff);
+    buff[len - 1] = '\0';
     while(strcmp(buff, "exit") != 0) {
         p = fork();
         if (p < 0) {
@@ -20,21 +31,25 @@ void terminal() {
             return;
         }
         if (p == 0) {
-            args[0] = buff;
-            args[1] = NULL;
-            res = execv(buff, args);
+            printf("%s", buff);
+            args[0] = strtok(buff, delim); // ruta 
+            for (int i = 0; args[i] != NULL; i++) {
+                args[i + 1] = strtok(NULL, delim); //demas arg.
+            }
+            res = execv(args[0], args);
             if( res < 0)
                 perror("Error al execvear!");
-            exit(-1);
+            exit(1);
         }
-        wait(&status);
+        wait(&status); // pid = 5
         if (WIFEXITED(status)) {
             printf("PROMPT$ ");
-            scanf("%s", buff);
+            fgets(buff, 50, stdin);
+            len = strlen(buff);
+            buff[len - 1] = '\0';
         } 
-        else 
+        else
             perror("Error retornar el nene!");
-        
     }
 }
 
